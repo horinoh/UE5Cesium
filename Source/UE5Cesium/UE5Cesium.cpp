1f859c323735d9b79cc841d40ba06d9210e7d9a5
horinoh <development@horino.sakura.ne.jp>
2023-06-04 15:52:32 +0900

    プロジェクト追加

---
+++ Source/UE5Cesium/UE5Cesium.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5Cesium.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5Cesium, "UE5Cesium" );
---
+++ Source/UE5Cesium/UE5CesiumGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5CesiumGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5CESIUM_API AUE5CesiumGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
